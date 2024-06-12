0b43b97d55f67beba33f0412b51acb77b7905c9f
Victor <victor@catio.tech>
2024-06-12 11:34:35 -0700

    adding in the relationship library

--- src/__examples__/index.ts
+++ src/__examples__/index.ts
@@ -1,3 +1,5 @@
 import componentLibraryJson from './component-library.json' assert { type: 'json' };
+import relationshipLibraryJson from './relationship-library.json' assert { type: 'json' };
 
 export const componentLibrary = componentLibraryJson;
+export const relationshipLibrary = relationshipLibraryJson;
--- src/__tests__/relationship-library.test.ts
+++ src/__tests__/relationship-library.test.ts
@@ -1,6 +1,6 @@
 import Ajv from 'ajv';
-import schema from '../component-library.schema.json';
-import { componentLibrary } from '../__examples__';
+import schema from '../relationship-library.schema.json';
+import { relationshipLibrary } from '../__examples__';
 
 const ajv = new Ajv();
 
@@ -12,12 +12,12 @@ it('should be a valid JSON Schema', () => {
 // Create validation function
 const validate = ajv.compile(schema);
 
-it(`should validate object "componentLibrary"`, () => {
-  const result = validate(componentLibrary);
+it(`should validate object "relationshipLibrary"`, () => {
+  const result = validate(relationshipLibrary);
 
   if (validate.errors) {
     throw new Error(
-      `Validation error in componentLibrary:\n\n${JSON.stringify(validate.errors, null, 2)}`
+      `Validation error in relationshipLibrary:\n\n${JSON.stringify(validate.errors, null, 2)}`
     );
   }
 
