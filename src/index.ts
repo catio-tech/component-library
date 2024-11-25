a9a62b929a6164c0afefb7eff16d48cc7619bb23
Matt Kharrl <matt@catio.tech>
2024-11-25 14:16:05 -0700

    export typed library and sorted keys

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import componentLibraryJsonSchema from './component-library.schema.json' assert { type: 'json' };
-import { componentLibrary } from './__examples__/index.js';
+import { componentLibrary as untypedComponentLibrary } from './__examples__/index.js';
 
 import relationshipLibraryJsonSchema from './relationship-library.schema.json' assert { type: 'json' };
 import { relationshipLibrary } from './__examples__/index.js';
+import { ComponentLibrary } from './types.js';
+
+const componentLibrary = untypedComponentLibrary as ComponentLibrary;
+
+const componentKeys = Object.keys(componentLibrary).sort();
 
 export {
   componentLibraryJsonSchema,
   componentLibrary,
+  componentKeys,
   relationshipLibraryJsonSchema,
   relationshipLibrary,
 };
