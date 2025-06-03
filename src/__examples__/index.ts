f76f947974ade89b17f0024647ae8f826e061acf
Devon Miller <devondrimmiemiller@gmail.com>
2025-06-03 15:12:56 -0400

    Expose Category Library

--- src/__examples__/index.ts
+++ src/__examples__/index.ts
@@ -1,5 +1,7 @@
 import componentLibraryJson from './component-library.json' assert { type: 'json' };
 import relationshipLibraryJson from './relationship-library.json' assert { type: 'json' };
+import categoryLibraryJson from './category-library.json' assert { type: 'json' };
 
 export const componentLibrary = componentLibraryJson;
 export const relationshipLibrary = relationshipLibraryJson;
+export const categoryLibrary = categoryLibraryJson;
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import componentLibraryJsonSchema from './component-library.schema.json' assert { type: 'json' };
 import { componentLibrary as untypedComponentLibrary } from './__examples__/index.js';
-
+import { categoryLibrary } from './__examples__/index.js';
 import relationshipLibraryJsonSchema from './relationship-library.schema.json' assert { type: 'json' };
 import { relationshipLibrary } from './__examples__/index.js';
 import { ComponentLibrary } from './types.js';
 
 const componentLibrary = untypedComponentLibrary as ComponentLibrary;
-
 const componentKeys = Object.keys(componentLibrary).sort();
 
 export {
   componentLibraryJsonSchema,
   componentLibrary,
   componentKeys,
+  categoryLibrary,
   relationshipLibraryJsonSchema,
   relationshipLibrary,
 };
